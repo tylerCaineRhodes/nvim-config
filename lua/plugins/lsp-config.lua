b6d2c5a032afa6d4bf336ed686397c38fc27949d
Tyler Rhodes <tyler.rhodes@strongmind.com>
2024-06-01 11:00:23 -0700

    Enable python lsps

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -14,15 +14,21 @@ return {
           "lua_ls",
           "tsserver",
           "html",
+          "jedi_language_server",
+          "pylsp",
+          "ruby_lsp",
+          'erb_formatter',
+          'prettier'
         },
       })
     end,
   },
   {
+
+    -- :LspInfo to see what language servers are running
     "neovim/nvim-lspconfig",
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
       local lspconfig = require("lspconfig")
 
       lspconfig.lua_ls.setup({
@@ -34,6 +40,16 @@ return {
       lspconfig.html.setup({
         capabilities = capabilities,
       })
+      lspconfig.jedi_language_server.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.pylsp.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.ruby_lsp.setup({
+        capabilities = capabilities,
+      })
+
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})

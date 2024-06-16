a2e617a963457a570feec1221eb3d90616b72c60
Tyler Rhodes <rhodetyl000@gmail.com>
2024-06-15 17:47:03 -0700

    Add noice for cool command line

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,26 @@
+return {
+  "folke/noice.nvim",
+  config = function()
+    require("noice").setup({
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            any = {
+              { find = "%d+L, %d+B" },
+              { find = "; after #%d+" },
+              { find = "; before #%d+" },
+              { find = "%d fewer lines" },
+              { find = "%d more lines" },
+            },
+          },
+          opts = { skip = true },
+        },
+      },
+    })
+  end,
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  },
+}

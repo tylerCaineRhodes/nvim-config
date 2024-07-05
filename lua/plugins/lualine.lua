2088501e2f350aae929afa10531bbe0f89f3af8a
Tyler Rhodes <rhodetyl000@gmail.com>
2024-07-04 18:00:23 -0700

    Show key commands in lua-line

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,6 +7,20 @@ return {
         -- other nice themes: base16, 16color, ayu_dark, codedark, iceberg_dark
         globalstatus = true,
       },
+      sections = {
+        lualine_x = {
+          {
+            require("noice").api.statusline.mode.get,
+            cond = require("noice").api.statusline.mode.has,
+            color = { fg = "#ff9e64" },
+          },
+          {
+            require("noice").api.status.command.get,
+            cond = require("noice").api.status.command.has,
+            color = { fg = "#ff9e64" },
+          },
+        },
+      },
     })
   end,
 }

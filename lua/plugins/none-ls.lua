2f4a586b9e9276611340217003a3cb3c61f7935c
Tyler Rhodes <tyler.rhodes@strongmind.com>
2024-08-04 14:24:03 -0700

    Address missing builtin for null-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -3,6 +3,7 @@ return {
   config = function()
     local null_ls = require("null-ls")
     null_ls.setup({
+      debug = true,
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
@@ -10,7 +11,6 @@ return {
         null_ls.builtins.formatting.isort,
         null_ls.builtins.formatting.rubocop,
         null_ls.builtins.diagnostics.rubocop,
-        null_ls.builtins.diagnostics.eslint_d,
       },
     })
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})

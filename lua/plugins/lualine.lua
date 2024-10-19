616a3069d1f0925b733acc7bc4d907a1180a5879
Tyler Rhodes <rhodetyl000@gmail.com>
2024-10-19 00:02:46 -0700

    Remove problematic error handling due to noice plugin

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -6,21 +6,7 @@ return {
         theme = "auto",
         -- other nice themes: base16, 16color, ayu_dark, codedark, iceberg_dark
         globalstatus = true,
-      },
-      sections = {
-        lualine_x = {
-          {
-            require("noice").api.statusline.mode.get,
-            cond = require("noice").api.statusline.mode.has,
-            color = { fg = "#ff9e64" },
-          },
-          {
-            require("noice").api.status.command.get,
-            cond = require("noice").api.status.command.has,
-            color = { fg = "#ff9e64" },
-          },
-        },
-      },
+      }
     })
   end,
 }

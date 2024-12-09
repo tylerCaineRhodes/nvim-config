367620eaf38105864db9aa1735f2591205ad76e0
Tyler Rhodes <rhodetyl000@gmail.com>
2024-12-08 18:48:57 -0700

    Improve lsp keybindings

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -48,7 +48,7 @@ return {
         capabilities = capabilities,
       })
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gh", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
     end,

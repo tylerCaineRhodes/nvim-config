40f591ce1131b4fdc6a1c2a3b2774c09783aeae3
Tyler Rhodes <rhodetyl000@gmail.com>
2025-01-12 15:06:53 -0700

    Reverse file results in telescope to address bug

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -20,11 +20,9 @@ return {
         entry_prefix = "  ",
         initial_mode = "insert",
         selection_strategy = "reset",
-        sorting_strategy = "ascending",
         layout_strategy = "horizontal",
         layout_config = {
           horizontal = {
-            prompt_position = "top",
             preview_width = 0.55,
             results_width = 0.8,
           },

2de08ea77c21143a4a2991d4de9be2da35245505
Tyler Rhodes <tyler.rhodes@strongmind.com>
2025-01-11 18:06:55 -0700

    Change auto-pairs plugin

--- lua/plugins/auto-pair.lua
+++ lua/plugins/auto-pair.lua
@@ -1,1 +1,9 @@
-return { "jiangmiao/auto-pairs" }
+return {
+  "windwp/nvim-autopairs",
+  event = "InsertEnter",
+  config = function()
+    require("nvim-autopairs").setup({
+      disable_filetype = { "TelescopePrompt" , "chatgpt-input", "markdown" },
+    })
+  end,
+}

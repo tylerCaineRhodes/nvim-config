82a9b2a816ccc69050d141d5ed0336ca9b608926
Tyler Rhodes <rhodetyl000@gmail.com>
2025-02-02 16:00:42 -0700

    Configure relative number colors

--- lua/plugins/github-nvim-theme.lua
+++ lua/plugins/github-nvim-theme.lua
@@ -11,5 +11,31 @@ return {
       },
     })
     vim.cmd("colorscheme github_dark_dimmed")
+
+    function set_rel_number_colors()
+      local normal_bg = vim.api.nvim_get_hl(0, { name = "LineNr" }).bg
+      local primary_fg = 'yellow'
+      local secondary_fg = 'magenta'
+
+      vim.cmd(string.format(
+        "highlight HL_RltvNmbr_Minus gui=none,italic guifg=%s guibg=%s",
+        primary_fg, normal_bg
+      ))
+      vim.cmd(string.format(
+        "highlight HL_RltvNmbr_Positive gui=none,italic guifg=%s guibg=%s",
+        secondary_fg, normal_bg
+      ))
+    end
+
+    set_rel_number_colors()
+
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      pattern = "*",
+      callback = function()
+        vim.schedule(function()
+          set_rel_number_colors()
+        end)
+      end,
+    })
   end,
 }

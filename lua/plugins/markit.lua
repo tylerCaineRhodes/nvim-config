502cadcff2a32b7070cd4302893154f58b2689c6
Tyler Rhodes <rhodetyl000@gmail.com>
2025-02-02 18:24:18 -0700

    Try marks_nvim

---
+++ lua/plugins/markit.lua
@@ -0,0 +1,26 @@
+return {
+  '2kabhishek/markit.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
+  config = function()
+    require('marks').setup({
+      default_mappings = true,
+      builtin_marks = { ".", "<", ">", "^" },
+      cyclic = true,
+      force_write_shada = false,
+      sign_priority = { lower=10, upper=15, builtin=8, bookmark=20 },
+      excluded_filetypes = {},
+      excluded_buftypes = {},
+      bookmark_0 = {
+        sign = " ",
+        virt_text = "follow the banana",
+        annotate = false,
+      },
+      bookmark_1 = {
+        sign = "🍌",
+        virt_text = "also a banana",
+        annotate = false,
+      },
+    })
+  end,
+}
+

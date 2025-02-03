e2abae701c1e76b47ab580445697813b2fafe683
Tyler Rhodes <rhodetyl000@gmail.com>
2025-02-02 19:21:58 -0700

    Do not show builtin marks by default

--- lua/plugins/markit.lua
+++ lua/plugins/markit.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     require('marks').setup({
       default_mappings = true,
-      builtin_marks = { ".", "<", ">", "^" },
+      -- builtin_marks = { ".", "<", ">", "^" },
       cyclic = true,
       force_write_shada = false,
       sign_priority = { lower=10, upper=15, builtin=8, bookmark=20 },

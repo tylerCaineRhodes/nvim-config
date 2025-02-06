a430277b8b8fae605c6daa8424939df344875007
Tyler Rhodes <rhodetyl000@gmail.com>
2025-02-06 00:10:19 -0700

    Add more shortcuts for chatgpt

--- lua/plugins/chatGPT.lua
+++ lua/plugins/chatGPT.lua
@@ -3,8 +3,20 @@ return {
   event = "VeryLazy",
   config = function()
     require("chatgpt").setup()
+    -- cursor-like
     vim.keymap.set("v", "<leader>k", ":ChatGPTEditWithInstructions<CR>", {})
     vim.keymap.set({ "n", "v" }, "<leader>l", ":ChatGPT<CR>", {})
+
+    vim.keymap.set({ "n", "v" }, "<leader>cg", ":ChatGPTRun grammar_correction<CR>", {})
+    vim.keymap.set({ "n", "v" }, "<leader>ct", ":ChatGPTRun translate<CR>", {})
+    vim.keymap.set({ "n", "v" }, "<leader>ck", ":ChatGPTRun keywords<CR>", {})
+    vim.keymap.set({ "n", "v" }, "<leader>cd", ":ChatGPTRun docstring<CR>", {})
+    vim.keymap.set({ "n", "v" }, "<leader>ca", ":ChatGPTRun add_tests<CR>", {})
+    vim.keymap.set({ "n", "v" }, "<leader>co", ":ChatGPTRun optimize_code<CR>", {})
+    vim.keymap.set({ "n", "v" }, "<leader>cs", ":ChatGPTRun summarize<CR>", {})
+    vim.keymap.set({ "n", "v" }, "<leader>cf", ":ChatGPTRun fix_bugs<CR>", {})
+    vim.keymap.set({ "n", "v" }, "<leader>cx", ":ChatGPTRun explain_code<CR>", {})
+    vim.keymap.set({ "n", "v" }, "<leader>cl", ":ChatGPTRun code_readability_analysis<CR>", {})
   end,
   dependencies = {
     "MunifTanjim/nui.nvim",

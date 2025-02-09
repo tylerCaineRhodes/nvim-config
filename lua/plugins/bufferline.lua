57ed4c4901f3e52cb0483b9cef451866bf62f8b8
Tyler Rhodes <rhodetyl000@gmail.com>
2025-02-09 12:41:02 -0700

    Configure bufferline options
    Not convinced I'll keep this plugin. I don't like how it overwrites
    normal tabs and mixes them up with buffers.

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -3,18 +3,23 @@ return {
   version = "*",
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function()
-    require('bufferline').setup{
+    require('bufferline').setup {
       options = {
+        numbers = "ordinal",
         themable = false,
         diagnostics = "nvim_lsp",
         show_tab_indicators = true,
         always_show_bufferline = false,
+        separator_style = "slant",
+        show_close_icon = false,
+        enforce_regular_tabs = true,
         offsets = {
           {
             filetype = "neo-tree",
           }
-        }
+        },
       }
     }
+    vim.api.nvim_set_keymap('n', 'bp', ':BufferLinePick<CR>', { noremap = true, silent = true })
   end,
 }

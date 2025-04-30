efc0896fa4625c68010601f1f130c9bda597cd02
Tyler Rhodes <rhodetyl000@gmail.com>
2025-04-30 14:43:42 -0700

    Remove unused plugins

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,12 +1,13 @@
 return {
-  { "EdenEast/nightfox.nvim",          lazy = true },
-  { "haishanh/night-owl.vim",          lazy = true },
-  { "ayu-theme/ayu-vim",               lazy = true },
-  { "chriskempson/vim-tomorrow-theme", lazy = true },
+  { "EdenEast/nightfox.nvim",           lazy = true },
+  { "haishanh/night-owl.vim",           lazy = true },
+  { "ayu-theme/ayu-vim",                lazy = true },
+  { "chriskempson/vim-tomorrow-theme",  lazy = true },
   { "folke/tokyonight.nvim" },
   { "tylercainerhodes/night-wolf.nvim" },
   {
     "projekt0n/github-nvim-theme",
+    lazy = true,
     config = function()
       require("github-theme").setup({
         options = {

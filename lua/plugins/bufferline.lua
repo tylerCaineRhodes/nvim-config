5d7eb778e4ec8f62cf0212668d9e754f787b293d
Tyler Rhodes <rhodetyl000@gmail.com>
2025-07-21 22:48:32 -0700

    Update bufferline behavior

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -15,16 +15,20 @@ return {
           show_tab_indicators = true,
           always_show_bufferline = false,
           separator_style = "slant",
-          show_close_icon = false,
+          show_close_icon = true,
           enforce_regular_tabs = true,
           offsets = {
             {
-              filetype = "neo-tree",
+              filetype = "neo-tree"
             }
           },
         }
       }
       vim.api.nvim_set_keymap('n', 'bp', ':BufferLinePick<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<leader>br', ':BufferLineCloseRight<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<leader>bl', ':BufferLineCloseLeft<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '[B', ':BufferLineMovePrev<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', ']B', ':BufferLineMoveNext<CR>', { noremap = true, silent = true })
     end
   end,
 }

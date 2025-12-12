83aaa8aacf97daa1589daee0966bd9536fd5af18
Tyler Rhodes <rhodetyl000@gmail.com>
2025-12-12 15:07:21 -0700

    Add navigation shortcuts for git changes

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -9,5 +9,33 @@ return {
       changedelete = { text = "~_" },
     },
     current_line_blame = true,
-  }
+  },
+  config = function(_, _opts)
+    local gs = require('gitsigns')
+    gs.setup({
+      on_attach = function(bufnr)
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        map('n', ']c', function()
+          if vim.wo.diff then
+            vim.cmd.normal({ ']c', bang = true })
+          else
+            gs.nav_hunk('next')
+          end
+        end)
+
+        map('n', '[c', function()
+          if vim.wo.diff then
+            vim.cmd.normal({ '[c', bang = true })
+          else
+            gs.nav_hunk('prev')
+          end
+        end)
+      end,
+    })
+  end,
 }

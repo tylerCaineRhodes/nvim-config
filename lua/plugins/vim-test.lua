e673f64f79d40ddb320181b5dd6113a021178f9b
Tyler Rhodes <rhodetyl000@gmail.com>
2026-02-27 17:11:56 -0700

    Improve vim-test syntax

--- lua/plugins/vim-test.lua
+++ lua/plugins/vim-test.lua
@@ -5,11 +5,25 @@ return {
       -- https://github.com/vim-test/vim-test?tab=readme-ov-file#strategies
       vim.g["test#strategy"] = "dispatch"
 
-      vim.keymap.set('n', 't<C-n>', '<cmd>TestNearest<CR>', { desc = 'Test nearest' })
-      vim.keymap.set('n', 't<C-f>', '<cmd>TestFile<CR>', { desc = 'Test file' })
-      vim.keymap.set('n', 't<C-s>', '<cmd>TestSuite<CR>', { desc = 'Test suite' })
-      vim.keymap.set('n', 't<C-l>', '<cmd>TestLast<CR>', { desc = 'Test last' })
-      vim.keymap.set('n', 't<C-g>', '<cmd>TestVisit<CR>', { desc = 'Test visit' })
+      vim.keymap.set('n', 't<C-n>', function()
+        vim.cmd.TestNearest()
+      end, { desc = 'Test nearest' })
+
+      vim.keymap.set('n', 't<C-f>', function()
+        vim.cmd.TestFile()
+      end, { desc = 'Test file' })
+
+      vim.keymap.set('n', 't<C-s>', function()
+        vim.cmd.TestSuite()
+      end, { desc = 'Test suite' })
+
+      vim.keymap.set('n', 't<C-l>', function()
+        vim.cmd.TestLast()
+      end, { desc = 'Test last' })
+
+      vim.keymap.set('n', 't<C-g>', function()
+        vim.cmd.TestVisit()
+      end, { desc = 'Test visit' })
     end,
   },
 }
